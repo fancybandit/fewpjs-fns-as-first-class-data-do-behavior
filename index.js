ae917221f86840ff0cedcc3afb489011bbfe67e0
Jacob Klimenko <droidedjake@gmail.com>
2021-06-19 21:53:14 -0400

    commit tree d39f33677653a873297fad4ee4495e15f8bf3c89

--- index.js
+++ index.js
@@ -9,5 +9,18 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(time) {
+  time = parseInt(time.split(":")[0]);
+  if (time < 12) {
+    return "Good Morning";
+  } else if (12 <= time && time <= 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  };
+};
+
+function displayMessage(message) {
+  const greeting = document.getElementById("greeting");
+  greeting.innerHTML = message;
+};
